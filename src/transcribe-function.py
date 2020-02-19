4e6fc61c8ddb695a0708a801e83467d0164e6991
Kanazawa <keikanaz@38f9d35d619c.ant.amazon.com>
2020-02-19 11:39:13 +0900

    1st commit

---
+++ src/transcribe-function.py
@@ -0,0 +1,24 @@
+import json
+import urllib.parse
+import boto3
+import datetime
+
+s3 = boto3.client('s3')
+transcribe = boto3.client('transcribe')
+
+def lambda_handler(event, context):
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    try:
+        transcribe.start_transcription_job(
+            TranscriptionJobName= datetime.datetime.now().strftime("%Y%m%d%H%M%S") + '_Transcription',
+            LanguageCode='en-US',
+            Media={
+                'MediaFileUri': 'https://s3.ap-northeast-1.amazonaws.com/' + bucket + '/' + key
+            },
+            OutputBucketName='YYYYMMDD-transcribe-output-YourName'
+        )
+    except Exception as e:
+        print(e)
+        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+        raise e
---
+++ src/translate-function-with-apigw.py
@@ -0,0 +1,22 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+
+    translate = boto3.client('translate')
+    input_text = event['queryStringParameters']['input_text']
+
+    response = translate.translate_text(
+        Text=input_text,
+        SourceLanguageCode='ja',
+        TargetLanguageCode='en'
+    )
+
+    output_text = response.get('TranslatedText')
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'output_text': output_text
+        })
+    }
